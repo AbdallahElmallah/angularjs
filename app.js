d01e367815e495eca14869324f0339bb08f3e219
abdallah youssef elmallah <32775832+abdallah-elmallah@users.noreply.github.com>
2021-12-23 17:26:46 +0200

    update module

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ var app = angular.module("LunchCheck", []);
 
 app.controller("LunchCheckController", function ($scope) {
     $scope.items = "";
-    $scope.message = "";
+    $scope.flag = false;
     $scope.msg = function () {
         if (
             $scope.items
                 .split(",")
                 .map((item) => item.replace(/\s/g, "").trim())
                 .filter((item) => item !== "").length === 0
-        )
+        ) {
             $scope.message = "Please enter data first";
+            $scope.checked = false;
+            $scope.flag = true;
+        }
         else if (
             $scope.items
                 .split(",")
                 .map((item) => item.replace(/\s/g, "").trim())
                 .filter((item) => item !== "").length <= 3
-        )
+        ) {
             $scope.message = "Enjoy!";
-        else $scope.message = "Too much!";
+            $scope.checked = true;
+            $scope.flag = true;
+
+        }
+        else {
+            $scope.message = "Too much!";
+            $scope.checked = true;
+            $scope.flag = true;
+
+        }
     };
 });
