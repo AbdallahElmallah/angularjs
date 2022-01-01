10eba7571f7e3d9277e503ed0f54ccb5b67458ab
abdallah youssef elmallah <32775832+abdallah-elmallah@users.noreply.github.com>
2022-01-01 22:52:20 +0200

    refactoring angularJS modules

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
+angular.module("LunchCheck", []);
+let app = angular.module("LunchCheck");
 
-let app = angular.module("LunchCheck", []);
-let app2 = angular.module("ShoppingList", []);
-let app3 = angular.module("ShoppingListCheckOff", []);
-let app4 = angular.module("ShoppingListPromiseApp", []);
-let app5 = angular.module('MenuCategoriesApp', []);
+angular.module("ShoppingList", []);
+let app2 = angular.module("ShoppingList");
+
+angular.module("ShoppingListCheckOff", []);
+let app3 = angular.module("ShoppingListCheckOff");
+
+angular.module("ShoppingListPromiseApp", []);
+let app4 = angular.module("ShoppingListPromiseApp");
+
+angular.module('MenuCategoriesApp', []);
+let app5 = angular.module('MenuCategoriesApp');
