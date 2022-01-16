64bba1ec67ee95adf864eccb25662253051c466c
abdallah youssef elmallah <32775832+abdallah-elmallah@users.noreply.github.com>
2022-01-16 12:41:23 +0200

    add sidebar

---
+++ src/dashboard/home/sideBar/sideBar.component.js
@@ -0,0 +1,8 @@
+dashboard.controller("SideBarController", SideBarController);
+dashboard.component('sideBar', {
+    templateUrl: "/src/dashboard/home/sideBar/sideBar.html",
+    controller: "SideBarController",
+    controllerAs: "sideBar",
+})
+
+function SideBarController() { }
